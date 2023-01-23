9552e02a9e2b99500bc4ab0299981cc53d995968
Toothi <73416381+Toothi@users.noreply.github.com>
2023-01-23 18:13:33 +0800

    Update qrcode.go

--- tools/qrcode.go
+++ tools/qrcode.go
@@ -2,8 +2,25 @@ package tools
 
 import (
 	"github.com/skip2/go-qrcode"
+	"strings"
 )
 
+func Checks(a string) string {
+	checkd1 := strings.Contains(a, "http://")
+	checkd2 := strings.Contains(a, "https://")
+	if checkd1 == true {
+		rename := string(a[7:])
+		return rename
+	} else if checkd2 == true {
+		rename := string(a[8:])
+		return rename
+	} else {
+		cuo := "ERROR"
+		return cuo
+	}
+}
+
 func Generate_QRcode(parameters string) {
-	qrcode.WriteFile(parameters, qrcode.Medium, 256, "./images/qrcode.png")
+	rename := Checks(parameters)
+	qrcode.WriteFile(parameters, qrcode.Medium, 256, "./images/"+rename+".png")
 }
