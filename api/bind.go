671f8f1c59064bc28b916a1af145b1bc6f6d8542
Toothi <73416381+Toothi@users.noreply.github.com>
2023-01-24 22:48:21 +0800

    Create bind.go

--- api/bind.go
+++ api/bind.go
@@ -19,17 +19,18 @@ func Response(w http.ResponseWriter, r *http.Request) {
 
 func Request_URL(a http.ResponseWriter, b *http.Request) {
 	a.Header().Set("Content-Type", "application/json")
+	a.WriteHeader(http.StatusOK)
 	dataGet := b.URL.Query()
 	Geturl := dataGet.Get("url")
 	ip := b.RemoteAddr
-	/* 调试输出
-	fmt.Println("dataGet: ", dataGet)
-	fmt.Println("Geturl: ", Geturl)
-	*/
 	log.Printf("[%s] [请求生成]: %s", ip, Geturl)
 	tools.Generate_QRcode(Geturl)
 	filename := tools.Checks(Geturl)
-	fmt.Fprintf(a, `{"status":%d,GetUrl":"%s","DownloadPath":"/images/%s.png"}`, http.StatusOK, Geturl, filename)
+	if filename == Geturl {
+		fmt.Fprintf(a, `{"status":%d,GetUrl":"%s","DownloadPath":"/images/%s.png","QRcode_type":"text"}`, http.StatusOK, Geturl, filename)
+	} else {
+		fmt.Fprintf(a, `{"status":%d,GetUrl":"%s","DownloadPath":"/images/%s.png","QRcode_type":"url"}`, http.StatusOK, Geturl, filename)
+	}
 	time.AfterFunc(25*time.Second, func() { //异步执行匿名函数
 		os.Remove("./images/" + filename + ".png")
 		log.Println("[INFO]: 已清理图像")
